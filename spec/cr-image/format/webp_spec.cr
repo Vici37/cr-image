39d9f2fb150edbd746183415b29f91997d87ba6a
Troy Sornson <troy@sornson.io>
2023-03-03 20:14:12 -0700

    Add webp support

---
+++ spec/cr-image/format/webp_spec.cr
@@ -0,0 +1,27 @@
+require "../../spec_helper"
+
+Spectator.describe CrImage::Format::WebP do
+  include SpecHelper
+
+  describe ".from_webp and #to_webp" do
+    it "works with GrayscaleImage" do
+      with_sample("scenic/moon.webp") do |io|
+        image = CrImage::GrayscaleImage.from_webp(io)
+        io = IO::Memory.new
+        image.to_webp(io)
+
+        digest(io.to_s).should eq "db79e8b578e4509dc4b2eaed010612ac2b1c1c32"
+      end
+    end
+
+    it "works with RGBAImage" do
+      with_sample("scenic/moon.webp") do |io|
+        image = CrImage::RGBAImage.from_webp(io)
+        io = IO::Memory.new
+        image.to_webp(io)
+
+        digest(io.to_s).should eq "308ce25679db4538c210831e613a7d5dd8a77b49"
+      end
+    end
+  end
+end
