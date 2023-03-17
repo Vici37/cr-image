10801a266181e88faaaf9463b4499f1d3c24b3de
Troy Sornson <troy@sornson.io>
2023-03-16 21:14:46 -0600

    Update digests per round->clamp change

--- spec/cr-image/operation/horizontal_blur_spec.cr
+++ spec/cr-image/operation/horizontal_blur_spec.cr
@@ -4,12 +4,12 @@ Spectator.describe CrImage::Operation::HorizontalBlur do
   include SpecHelper
 
   specs_for_operator(horizontal_blur(10),
-    gray_hash: "1d4edded3e030beba3527e9b5757b1407e3f75db",
-    rgba_hash: "cf48769241b22392925cd62724a6cc631e618572"
+    gray_hash: "f8ef00c6475a4a10b1fca8e428dfb1c4590ca2a5",
+    rgba_hash: "d2fa1509a3515af24ee4c21980a8ee224a1ab1b0"
   )
 
   specs_for_operator(horizontal_blur!(10),
-    gray_hash: "1d4edded3e030beba3527e9b5757b1407e3f75db",
-    rgba_hash: "cf48769241b22392925cd62724a6cc631e618572"
+    gray_hash: "f8ef00c6475a4a10b1fca8e428dfb1c4590ca2a5",
+    rgba_hash: "d2fa1509a3515af24ee4c21980a8ee224a1ab1b0"
   )
 end
--- spec/cr-image/operation/vertical_blur_spec.cr
+++ spec/cr-image/operation/vertical_blur_spec.cr
@@ -4,12 +4,12 @@ Spectator.describe CrImage::Operation::VerticalBlur do
   include SpecHelper
 
   specs_for_operator(vertical_blur(10),
-    gray_hash: "192ec915c4f111faba16fcb16de7cd615afaf970",
-    rgba_hash: "fd658a5f7a2e815b493dcd30d04601a05da7609c"
+    gray_hash: "3b6ebdd3da8daf97b28ff76755f99fda08237585",
+    rgba_hash: "09fb58df58a8368b26dccc2078b8e31afeeaf512"
   )
 
   specs_for_operator(vertical_blur!(10),
-    gray_hash: "192ec915c4f111faba16fcb16de7cd615afaf970",
-    rgba_hash: "fd658a5f7a2e815b493dcd30d04601a05da7609c"
+    gray_hash: "3b6ebdd3da8daf97b28ff76755f99fda08237585",
+    rgba_hash: "09fb58df58a8368b26dccc2078b8e31afeeaf512"
   )
 end
