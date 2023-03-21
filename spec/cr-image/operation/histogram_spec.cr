8f372b0ddcae567d9d73132de1ead93375411efd
Troy Sornson <troy@sornson.io>
2023-03-21 08:08:09 -0600

    Add specs for histogram operation

--- spec/cr-image/operation/histogram_spec.cr
+++ spec/cr-image/operation/histogram_spec.cr
@@ -4,25 +4,13 @@ require "benchmark"
 Spectator.describe CrImage::Operation::Histogram do
   include SpecHelper
 
-  let(image) { CrImage::GrayscaleImage.open("docs/images/sample.jpg") }
+  specs_for_operator(histogram_equalize_image,
+    gray_hash: "5109c626e06ad5704586ce0533e89d7e245f27f9",
+    rgba_hash: "2e9f59da8d221654f4e1b4446539d1d6bb648c9b"
+  )
 
-  it "histograms", :focus do
-    # gray = image.threshold(8).region.crop(image)
-    # gray = CrImage::GrayscaleImage.open("test.png")
-    gray = CrImage::GrayscaleImage.open("mt_fuji.jpg")
-    # pp! gray.histogram(:gray)
-    # pp! gray.normalized_histogram(:gray)
-    # pp! gray.cumulative_distribution_histogram(:gray)
-    # pp! gray.histogram_equalize(:gray)
-    gray.save("original.png")
-    equalized = gray.histogram_equalize_image.save("equalized.png")
-    # pp! gray.histogram_equalize_image.normalized_histogram(:gray)
-    gray.draw_histogram(:gray).save("original_histogram.png")
-    equalized.draw_histogram(:gray).save("equalized_histogram.png")
-    gray.contrast(10).save("contrast.png")
-    # verify = CrImage::GrayscaleImage.open("test.png")
-    # equalized.gray.each_with_index do |val, i|
-    #   pp! val, verify.gray[i]
-    # end
-  end
+  specs_for_operator(histogram_equalize_image!,
+    gray_hash: "5109c626e06ad5704586ce0533e89d7e245f27f9",
+    rgba_hash: "2e9f59da8d221654f4e1b4446539d1d6bb648c9b"
+  )
 end
