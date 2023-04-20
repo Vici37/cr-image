69952ee0d4b409c83dc894a316d6bbef1545661a
Troy Sornson <troy@sornson.io>
2023-04-19 21:06:30 -0600

    Add shape method

--- spec/cr-image/map_spec.cr
+++ spec/cr-image/map_spec.cr
@@ -194,8 +194,11 @@ Spectator.describe CrImage::Map do
         [-1.0, 0.0, 1.0],
         [2.0, 3.0, 4.0],
         [5.0, 6.0, 7.0],
-
       ])
+    end
+
+    it "has a shape" do
+      expect(map.shape).to eq({3, 3})
     end
 
     it "converts to GrayscaleImage without scaling" do
