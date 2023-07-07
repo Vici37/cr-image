950c7f266ab8daf1b6e757911695aaac836584fa
Troy Sornson <troy@sornson.io>
2023-07-07 06:56:46 -0600

    Add documentation for padding operation, fix clone bug

--- src/cr-image/operation/pad.cr
+++ src/cr-image/operation/pad.cr
@@ -1,6 +1,19 @@
+# Pads an image
+#
+# Taking sample `image`:
+#
+# <img src="https://raw.githubusercontent.com/Vici37/cr-image/master/docs/images/sample.jpg" alt="Woman with black turtleneck and white background"/>
+#
+# ```
+# image.pad(left: 50, right: 50)
+# image.pad(left: 50, right: 50, pad_type: CrImage::EdgePolicy::Repeat)
+# ```
+# ```
+# <img src="https://raw.githubusercontent.com/Vici37/cr-image/master/docs/images/pad_black.jpg" alt="Picture with black padding on left and right"/>
+# <img src="https://raw.githubusercontent.com/Vici37/cr-image/master/docs/images/pad_repeat.jpg" alt="Picture with repeated padding on left and right"/>
 module CrImage::Operation::Pad
   def pad(all : Int32 = 0, *, top : Int32 = 0, bottom : Int32 = 0, left : Int32 = 0, right : Int32 = 0, pad_type : EdgePolicy = EdgePolicy::Black) : self
-    pad!(all, top: top, bottom: bottom, left: left, right: right, pad_type: pad_type)
+    clone.pad!(all, top: top, bottom: bottom, left: left, right: right, pad_type: pad_type)
   end
 
   def pad!(all : Int32 = 0, *, top : Int32 = 0, bottom : Int32 = 0, left : Int32 = 0, right : Int32 = 0, pad_type : EdgePolicy = EdgePolicy::Black) : self
